1e15d4694f9f4c3965cce92bb658fa3dbd285161
Janice Yan <yanj12@mcmaster.ca>
2024-02-01 20:44:47 -0500

    Added a geom_jitter() layer to all the boxplots and updated the analyses.

--- males_fall_2022/scripts/individual_male_analyses.R
+++ males_fall_2022/scripts/individual_male_analyses.R
@@ -7,6 +7,7 @@ library(car)
 library(igraph)
 library(ggsci)
 library(netdiffuseR)
+library(glmmTMB)
 
 My_Theme = theme(
   axis.title.x = element_text(size = 20),
@@ -26,73 +27,64 @@ male_data$replicate <- as.factor(male_data$replicate)
 ## Plot data
 ## 1) Proportion of mounts directed at other males
 ggplot(data = male_data, aes(x = treatment, y = prop_male, fill = treatment)) + 
-      geom_boxplot() + labs(y = "Male mount rate", x = NULL) + ylim(0, 1) +
-      My_Theme + scale_fill_manual(values =c("lightblue1", "deepskyblue4"))
+      geom_boxplot(outlier.colour = NA) + labs(y = "Male mount rate", x = NULL) + ylim(0, 1) +
+      My_Theme + scale_fill_manual(values =c("lightblue1", "deepskyblue4")) + 
+      geom_jitter(width = 0.1, alpha = 0.3)
 
 ## 2) Proportion of mounts where females attempted to avoid that were successful
 ggplot(data = male_data, aes(x = treatment, y = avoid_success_rate, fill = treatment)) + 
-       geom_boxplot() + labs(y = "Female escape rate", x = NULL) + ylim(0, 1) +
-       My_Theme + scale_fill_manual(values =c("lightblue1", "deepskyblue4"))
+       geom_boxplot(outlier.colour = NA) + labs(y = "Female escape rate", x = NULL) + ylim(0, 1) +
+       My_Theme + scale_fill_manual(values = c("lightblue1", "deepskyblue4")) +
+       geom_jitter(width = 0.1, alpha = 0.3)
 
 ## 3) Insemination rate (inseminations per)
 ggplot(data = male_data, aes(x = treatment, y = insem_rate, fill = treatment)) + 
-       geom_boxplot(fatten = 3) + labs(y = "Inseminations per day", x = NULL) + 
-       My_Theme + scale_fill_manual(values =c("lightblue1", "deepskyblue4"))
+       geom_boxplot(fatten = 3, outlier.colour = NA) + labs(y = "Inseminations per day", x = NULL) + 
+       My_Theme + scale_fill_manual(values =c("lightblue1", "deepskyblue4")) + 
+       geom_jitter(width = 0.1, alpha = 0.3)
 
 ## 4) Mount rate
 ggplot(data = male_data, aes(x = treatment, y = mount_rate, fill = treatment)) + 
-       geom_boxplot() + labs(y = "Mounts per day", x = NULL) + 
-       My_Theme + scale_fill_manual(values =c("lightblue1", "deepskyblue4"))
+       geom_boxplot(outlier.colour = NA) + labs(y = "Mounts per day", x = NULL) + 
+       My_Theme + scale_fill_manual(values =c("lightblue1", "deepskyblue4")) + 
+       geom_jitter(width = 0.1, alpha = 0.3)
 
 ## 5) Opposite sex networks
 ggplot(data = male_data, aes(x = treatment, y = oppo_sex_strength, fill = treatment)) + 
-  geom_boxplot() + labs(y = "Opposite-sex strength", x = NULL) + 
-  My_Theme + scale_fill_manual(values =c("lightblue1", "deepskyblue4"))
-
+       geom_boxplot(outlier.colour = NA) + labs(y = "Opposite-sex strength", x = NULL) + 
+       My_Theme + scale_fill_manual(values =c("lightblue1", "deepskyblue4")) +
+       geom_jitter(width = 0.1, alpha = 0.3)
 
 ################################## MODELS #########################################################
 ## 1) Proportion of mounts directed at other males
-all_data_male_mounts <- read.csv("males_fall_2022/data/all_data_combined.csv") %>% 
-                        filter(behaviour == "mount" | behaviour == "insemination")  # removes one data-entry where behaviour was "other"
-                       
-all_data_male_mounts$behaviour <- as.factor(all_data_male_mounts$behaviour)
-all_data_male_mounts$partner_sex <- as.factor(all_data_male_mounts$partner_sex)
+male_summary_data <- read.csv("males_fall_2022/data/combined_individual_data.csv", stringsAsFactors = TRUE) %>% 
+                     filter(day == 1 | day == 2) 
 
-# Model
-male_mount_mod <- glmer(data = all_data_male_mounts, partner_sex ~ treatment + (1|replicate) +
-                   (1|replicate:patch_focal), family = binomial())
+male_mount_mod <- glmer(data = male_summary_data, cbind(male_mounts, female_mounts) ~ treatment*day + (1|replicate/ID), 
+                          family = binomial())
 
-plot(simulateResiduals(male_mount_mod))
+plot(simulateResiduals(male_mount_mod)) # Looks good
 summary(male_mount_mod)
 Anova(male_mount_mod)
 
-
 ## 2) Proportion of mounts where females attempted to avoid that were successful
-all_data_avoid <- read.csv("males_fall_2022/data/all_data_combined.csv") %>% 
-                  filter(behaviour == "mount" | behaviour == "insemination") %>%  # removes one data-entry where behaviour was "other"
-                  filter(partner_avoid...Y.N. == "y" | partner_avoid...Y.N. == "Y") # filters for only cases where females attempted to avoid
+female_avoid_model <- glmer(data = male_data, cbind((attempted_avoid - mounts_evaded), mounts_evaded) ~ treatment*day + 
+                            (1|replicate/ID), family = binomial())
 
-all_data_avoid$avoid_success[all_data_avoid$avoid_success == "Y"] <- "y" # Turns uppercase to lowercase
-all_data_avoid$avoid_success[all_data_avoid$avoid_success == "N"] <- "n" # Turns uppercase to lowercase
+plot(simulateResiduals(female_avoid_model)) # Looks good
+summary(female_avoid_model)
+summary(female_avoid_model)
 
-all_data_avoid$avoid_success <- as.factor(all_data_avoid$avoid_success)
- 
-# Model
-avoid_model <- glmer(data = all_data_avoid, avoid_success ~ treatment + (1|replicate) + 
-                                         (1|replicate:patch_focal), family = binomial())
-plot(simulateResiduals(avoid_model))
-summary(avoid_model)
-Anova(avoid_model)
-
-## 3) Insemination rate (inseminations per day)
-insem_model <- lmer(data = male_data, insem_rate ~ treatment + (1|replicate))
+## 3) Insemination rate (inseminations per day) - I used glmmTMB instead of glmer here bc I got a singular fit with glmer
+insem_model <- glmmTMB(data = male_summary_data, inseminations ~ treatment*day + 
+                      (1|replicate/ID), family = nbinom2())
 
 plot(simulateResiduals(insem_model))
 Anova(insem_model)
 summary(insem_model)
 
 ## 3) Mount rate (Mounts per day)
-mount_model <- lmer(data = male_data, mount_rate ~ treatment + (1|replicate))
+mount_model <- glmmTMB(data = male_summary_data, mounts ~ treatment*day + (1|replicate/ID), family = nbinom2())
 
 plot(simulateResiduals(mount_model))
 summary(mount_model)
